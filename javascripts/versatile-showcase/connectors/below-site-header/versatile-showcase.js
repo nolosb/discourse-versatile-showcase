7dd5aad0fc361c529ad75b1d575acf38d4426b90
Nolo <26887899+nolosb@users.noreply.github.com>
2022-12-21 11:48:09 +0000

    refactor(connectors): simplify outlet conditional

--- javascripts/versatile-showcase/connectors/below-site-header/versatile-showcase.js
+++
@@ -1,9 +0,0 @@
-export default {
-  setupComponent(args, component) {
-    if (settings.plugin_outlet == "below-site-header") {
-      this.set("belowHeader", true);
-    } else {
-      this.set("belowHeader", false);
-    }
-  },
-};
--- javascripts/versatile-showcase/connectors/discovery-list-container-top/versatile-showcase.js
+++
@@ -1,9 +0,0 @@
-export default {
-  setupComponent(args, component) {
-    if (settings.plugin_outlet == "discovery-list-container-top") {
-      this.set("discoveryList", true);
-    } else {
-      this.set("discoveryList", false);
-    }
-  },
-};
